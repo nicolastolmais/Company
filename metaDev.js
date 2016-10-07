3714f27bf1475c565c5b7c2e6fef6179efb07895
CORPORATE\NPT0726 <nic.tolmais@daugherty.com>
2016-10-07 15:58:23 -0500

    worked on scroll button that still doesnt work. Worked on css of About page

--- metaDev.js
+++ metaDev.js
@@ -1,53 +1,23 @@
-/* When the user clicks on the button,
-toggle between hiding and showing the dropdown content */
-function myFunction() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
-function myFunction2(){
-   document.getElementById("myDropdown2").classList.toggle("show");
-}
-function showValue(newValue)
-{
-	document.getElementById("minrange").innerHTML=newValue;
-}
-function showValue2(newValue)
-{
-	document.getElementById("maxrange").innerHTML=newValue;
-}
-
-
-// Close the dropdown if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
-window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn2')) {
-
-    var dropdowns = document.getElementsByClassName("dropdown-content2");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
-      }
-    }
-  }
-}
-
+$( document ).ready(function() {
+  
 //javascript if browser width is less x than change css
-$(function(){
-  $(window).on("load resize", function() {
+$(function () {
+  $(window).on("load resize", function () {
     $(".backgroundPic").css("height", window.innerHeight);
+  }); 
+
+
+
+  //Smooth Scrolling
+  $('.Introduction a').bind('click', function () {
+    $('html, body').stop().animate({
+      scrollTop: $($(this).attr('href')).offset().top
+    }, 1500, 'easeInOutExpo');
+    event.preventDefault();
   });
+});
+
+//Adding carousel
+$('.carousel').carousel();
+
 });
